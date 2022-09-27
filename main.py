e97dfc3d89b6fbefaa9f5b51bef3ecce723507fc
CptDarkrex <rodrigcp22@gmail.com>
2022-09-27 13:41:13 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,252 @@
+#===================================================#
+#=  Project Tomori                                 =#
+#=  Bot for personal use the zerotwo alternative   =#
+#=  Created by Carlos Rodriguez                    =#
+#=  Copyright reserved Lubris                      =#
+#===================================================#
+
+#   ===== Discord Setup =====   #
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+#   ===== Pillow Setup =====   #
+from PIL import Image, ImageFont, ImageDraw
+from pathlib import Path
+
+#   ===== MariaDB Dependencies =====   #
+import mysql.connector as mariadb
+
+#   ===== Music Function Dependencies ===== #
+import youtube_dl
+import asyncio
+import os
+voice_clients = {}
+
+yt_dl_opts = {'format': 'bestaudio/best'}
+ytdl = youtube_dl.YoutubeDL(yt_dl_opts)
+
+ffmpeg_options = {'options': "-vn"}
+
+
+
+#   ===== Maria DB =====    #
+mariadb_connection = mariadb.connect(user='root', password='zqf3afr1GVQ9uae-cwe', host='10.20.10.35', port='3306', database= 'kei_database')
+create_cursor = mariadb_connection.cursor()#dictionary=True <--- can make the database into dictionary format
+
+#   ===== Create Database ===== #
+# create_cursor.execute("CREATE DATABASE kei_database")
+
+# create_cursor.execute("SHOW DATABASES")
+# for x in create_cursor:
+#     print(x)
+
+#   ===== Create Tables =====   #
+# create_cursor.execute("CREATE TABLE profile_card_users (usr_id BIGINT, name VARCHAR(24), birth DATE, description VARCHAR(250), cus_img_url VARCHAR(9000))")
+
+create_cursor.execute("SHOW TABLES")
+for x in create_cursor:
+    print(x)
+
+#   ===== Delete Table =====    #
+# create_cursor.execute("DROP TABLE profile_card_users")
+
+#   ===== Profile Insert into Database ===== #
+def profile_data(usr_id, name, birth, desc, img_id):
+    sql_cd = "INSERT INTO profile_card_users (usr_id, name, birth, description, cus_img_url) VALUES (%s, %s, %s, %s, %s)"
+    sql_values = (usr_id, name, birth, desc, img_id)
+    create_cursor.execute(sql_cd, sql_values)
+    mariadb_connection.commit()
+
+print(create_cursor.rowcount)
+
+#   ===== Testing Some random Shanonigans ===== #
+# sql_statement = "SELECT * FROM profile_card_users WHERE usr_id = %s"
+# usr = (570091674559315968,)
+# create_cursor.execute(sql_statement, usr)
+# my_result = create_cursor.fetchall()
+# my_tuple = my_result[0]
+# namo = my_tuple[1]
+#
+# print(namo)
+
+
+# ===== Profile Drawing Functions =====  #
+def profile_create(usr):
+    # creates a new image copy to work in
+    if Path(f"{usr}.jpg").is_file() == False:
+        my_image = Image.open("images/yuu.jpg")
+        my_image.save(f"completed_profiles/{usr}_template.jpg")
+
+    user = (usr,)
+    sql_statement = "SELECT * FROM profile_card_users WHERE usr_id = %s"
+    create_cursor.execute(sql_statement, user)
+    my_result = create_cursor.fetchall()
+    my_tuple = my_result[0]
+    name = my_tuple[1]
+    birth = my_tuple[2]
+    desc = my_tuple[3]
+
+    profile_name(usr, name, f"completed_profiles/{usr}_template.jpg")
+    # profile_birth(usr, birth, f"{usr}.jpg")
+    profile_description_text(usr, desc, f"completed_profiles/{usr}_template2.jpg")
+
+def profile_name(user, name, img_path):
+    # my_image = Image.open(f"{user}.jpg")
+    # if Path(f"{user}.jpg").is_file() == False:
+    #     my_image = Image.open("images/yuu.jpg")
+    # if Path(f"{user}.jpg").is_file() == True:
+    #     my_image = Image.open(f"{user}.jpg")
+    my_image = Image.open(img_path)
+
+    title_font = ImageFont.truetype("fonts/IndieFlower-Regular.ttf", size=28)
+    title_text = f"Name: {name}"
+    image_editable = ImageDraw.Draw(my_image)
+    image_editable.text((15, 15), title_text, (237, 230, 211), font=title_font)
+    my_image.save(f"completed_profiles/{user}_template2.jpg")
+
+def profile_birth(user, birth, img_path):
+    # my_image = Image.open(f"{user}.jpg")
+    # edit_user_image = ""
+    # my_image = ""
+    # if Path(f"{user}.jpg").is_file() == False:
+    #     my_image = Image.open("images/yuu.jpg")
+    # if Path(f"{user}.jpg").is_file() == True:
+    #     my_image = Image.open(f"{user}.jpg")
+    my_image = Image.open(img_path)
+
+    title_font = ImageFont.truetype("fonts/IndieFlower-Regular.ttf", size=28)
+    birth_text = birth
+    image_editable = ImageDraw.Draw(my_image)
+    image_editable.text((15,60), birth_text, (237, 230, 211), font=title_font)
+    my_image.save(f"completed_profiles/{user}.jpg")
+
+def profile_description_text(user, desc, img_path):
+    # if Path(f"{user}.jpg").is_file() == False:
+    #     my_image = Image.open("images/yuu.jpg")
+    # if Path(f"{user}.jpg").is_file() == True:
+    #     my_image = Image.open(f"{user}.jpg")
+    my_image = Image.open(img_path)
+
+    title_font = ImageFont.truetype("fonts/IndieFlower-Regular.ttf", size=28)
+    title_text = f"Description: {desc}"
+    image_editable = ImageDraw.Draw(my_image)
+    image_editable.text((15, 50), title_text, (237, 230, 211), font=title_font)
+    my_image.save(f"completed_profiles/{user}.jpg")
+
+#  ===== Embed Template =====  #
+embed = discord.Embed(title="Sample Embed", url="https://realdrewdata.medium.com/",
+                      description="This is an embed that will show how to build an embed and the different components",
+                      color=0xFF5733, )
+embed.set_image(url="https://c4.wallpaperflare.com/wallpaper/771/807/811/anime-anime-girls-zero-two-zero-two-darling-in-the-franxx-wallpaper-preview.jpg")
+embed.add_field(name="Field 1 Title", value="This is the value for field 1. This is NOT an inline field.", inline=False)
+embed.add_field(name="Field 2 Title", value="This is the value for field 1. This is NOT an inline field.", inline=False)
+
+
+#   ===== Set up =====  #
+class aclient(discord.Client):
+    def __init__(self):
+        super().__init__(intents = discord.Intents.default())
+        self.synced = False #we use this so the bot doesn't sync commands more than once
+
+    async def on_ready(self):
+        await self.wait_until_ready()
+        if not self.synced: #check if slash commands have been synced
+            await tree.sync(guild = discord.Object(id=944147234562400256)) #guild specific: leave blank if global (global registration can take 1-24 hours)
+            self.synced = True
+        print(f"We have logged in as {self.user}.")
+
+client = aclient()
+tree = app_commands.CommandTree(client)
+
+#   ==========================  #
+#   ===== Slash Commands =====  #
+#   ==========================  #
+@tree.command(guild = discord.Object(id=944147234562400256), name = 'embed', description='testing') #guild specific slash command
+async def slash2(interaction: discord.Interaction):
+    await interaction.response.send_message(f"I am working ! I was made with Discord.py!", ephemeral = False, embed=embed)
+
+@tree.command(guild = discord.Object(id=944147234562400256), name = 'hi', description='testing') #guild specific slash command
+async def slash2(interaction: discord.Interaction):
+    await interaction.response.send_message(f"I am working! I was made with Discord.py!", ephemeral = False)
+
+@tree.command(guild = discord.Object(id=944147234562400256), name = 'profile', description='A profile card of who you are accross all servers with this bot in it.') #guild specific slash command
+async def slash2(interaction: discord.Interaction):
+    user = interaction.user.id
+    profile_create(user)
+    await interaction.response.send_message(ephemeral = False, file=discord.File(f"completed_profiles/{user}.jpg"))
+
+@tree.command(guild = discord.Object(id=944147234562400256), name = 'create_profile', description='creates a profile card. Birth is in YY/MM/DD format. Paste a URL in img_id (optional)') #guild specific slash command
+async def slash2(interaction: discord.Interaction, name:str, description:str, birth:str, img_id:str):
+    user_id = interaction.user.id
+    # profile_name(user_id, name)
+    # profile_description_text(user_id, description)
+
+    profile_data(user_id, name, birth, description, img_id)
+
+    await interaction.response.send_message(f"Success! created you profile!", ephemeral=False)
+
+
+#   ===== Music Commands =====  #
+@tree.command(guild = discord.Object(id=944147234562400256), name = 'play', description='testing') #guild specific slash command 944147234562400256
+async def slash2(interaction: discord.Interaction, url:str):
+    try:
+        voice_client = await interaction.user.voice.channel.connect()
+        voice_clients[voice_client.guild.id] = voice_client
+    except:
+        print("error")
+
+    try:
+
+        loop = asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=False))
+
+        song = data['url']
+        player = discord.FFmpegPCMAudio(song, **ffmpeg_options, executable="C:\\ffmpeg-2022\\bin\\ffmpeg.exe")
+
+        voice_clients[interaction.user.guild.id].play(player)
+
+    except Exception as err:
+        print(err)
+    await interaction.response.send_message(f"I am working! I was made with Discord.py!", ephemeral = False)
+
+@tree.command(guild = discord.Object(id=944147234562400256), name = 'pause', description='testing') #guild specific slash command 944147234562400256
+async def slash2(interaction: discord.Interaction):
+    try:
+        voice_clients[interaction.user.guild.id].pause()
+    except Exception as err:
+        print(err)
+    await interaction.response.send_message(f"I am working! I was made with Discord.py!", ephemeral = False)
+
+@tree.command(guild=discord.Object(id=944147234562400256), name='resume',description='testing')  # guild specific slash command
+async def slash2(interaction: discord.Interaction):
+    try:
+        voice_clients[interaction.user.guild.id].resume()
+    except Exception as err:
+        print(err)
+    await interaction.response.send_message(f"I am working! I was made with Discord.py!", ephemeral=False)
+
+@tree.command(guild = discord.Object(id=944147234562400256), name = 'stop', description='testing') #guild specific slash command
+async def slash2(interaction: discord.Interaction):
+    try:
+        voice_clients[interaction.user.guild.id].stop()
+        await voice_clients[interaction.user.guild.id].disconnect()
+    except Exception as err:
+        print(err)
+    await interaction.response.send_message(f"I am working! I was made with Discord.py!", ephemeral = False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.run('OTY0MzY4NDg3NjAwNDk2Njgw.Glb6te.iSHY8O_AP_OnbclOPwHsRsjOwkZUYjuD8A-tOs')
